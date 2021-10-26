4b12f2477d25596975a3c2b2560842177ef12927
Ajay <ajay963@outlook.com>
2021-10-26 22:28:29 +0530

    fixed distance btw nodes

--- Tree/nodesDis.cpp
+++ Tree/nodesDis.cpp
@@ -14,45 +14,45 @@ class Node{
     }
 };
 
-Node* LCA(Node* root,int n1,int n2){
+Node* lca(Node* root,int n1,int n2){
   if( root == NULL)
   return NULL;
 
    if( root->data == n1 || root->data == n2)
    return root;
    
-   Node* left = LCA(root->left,n1,n2);
-   Node* right  = LCA(root->right,n1,n2);
+   Node* left = lca(root->left,n1,n2);
+   Node* right  = lca(root->right,n1,n2);
 
    if( left != NULL && right != NULL)
    return root;
 
-   if( root->left == NULL & root->right ==NULL)
-   return NULL;
+//    if( left == NULL && right == NULL)
+//    return NULL;
 
-   if( root->left != NULL)
-   return LCA(root->left,n1,n2);
+   if( left != NULL)
+   return left;
    
-   return LCA(root->right,n1,n2);
+   return right;
 }
 
-int treeDist(Node *root,int k,int dist){
+int treeDist(Node *root,int k,int level){
     if(root == NULL)
     return -1;
 
     if(root->data == k)
-    return dist;
+    return level;
 
-    int left = treeDist(root->left,k,dist+1);
+    int left = treeDist(root->left,k,level+1);
 
     if( left != -1)
     return left;
 
-    return treeDist(root->right,k,dist+1);
+    return treeDist(root->right,k,level+1);
 }
 
 int disBtwNode(Node* root,int n1,int n2){
-    Node* node = LCA(root,n1,n2);
+    Node* node = lca(root,n1,n2);
 
     int d1 = treeDist(node,n1,0);
     int d2 = treeDist(node,n2,0);
@@ -70,6 +70,6 @@ int main(){
     root->right->right->left = new Node(10);
     root->left->right->right = new Node(12);
 
-    cout<<disBtwNode(root,4,10);
+    cout<<disBtwNode(root,10,12);
     return 0;
 }
