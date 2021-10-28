45fe92d303c31f6640e1e33495534539eefee6f7
Ajay <ajay963@outlook.com>
2021-10-28 22:21:35 +0530

    Binary search tree

---
+++ smallestEle.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+class TreeNode{
+    public:
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int k){
+    val = k;
+    left = right = NULL;
+    }
+};
+
+// int kthsmallest(TreeNode* root,int k){
+//     if( root == NULL)
+//     return 0;
+
+//     if(root->val < )
+// }
+
+int main(){
+
+    return 0;
+}
