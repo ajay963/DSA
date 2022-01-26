9385f32d3d42bd4f66b3e6d8046fd423552412a8
Ajay Manjhi <43930202+ajay963@users.noreply.github.com>
2022-01-26 10:28:12 +0530

    practicing linkedlist

--- linklist/template.cpp
+++ linklist/template.cpp
@@ -1,34 +1,74 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int makeItEqual(int a, int b, int c) {
-    int ctr = 0;
+class Node{
+    public:
+    int data;
+    Node* next;
 
-    while(c>0 or a>0 or b>0){
-        if(c%2==1){
-           if(a%2==0)
-               ctr++;
-
-           if(b%2==0)
-           ctr++;
-        }
-        else{  
-            if(a%2==1 and b%2==1)
-            ctr++;
-        }
-        c=c/2;
-        a=a/2;
-        b=b/2;
+    Node(int x){
+        data = x;
+        next =NULL;
     }
-    return ctr;
+};
+
+    Node* segregate(int N, Node *head){
+        Node* evSt = NULL;
+        Node* evPtr = NULL;
+        Node* oddSt = NULL;
+        Node* oddPtr = NULL;
+        
+        Node* head1 = head;
+        while(head!=NULL){
+            if(head->data%2==0){
+                if(evSt==NULL){
+                    evSt = head;
+                    evPtr = head;
+                }else{
+                    evPtr->next = head;
+                    evPtr = evPtr->next;
+                }
+            }else{
+                if(oddSt==NULL){
+                    oddSt = head;
+                    oddPtr = head; 
+                }else{
+                    oddPtr->next = head;
+                    oddPtr = oddPtr->next;
+                }
+            }
+        head = head->next;
+        }
+        oddPtr->next = NULL;
+        if(evSt==NULL)
+        return head1; 
+        evPtr->next = oddSt;
+        return evSt ;
+    }
+
+void display(Node* head){
+    while(head!=NULL){
+        cout<<head->data<<" ";
+        head = head->next;
+    }
 }
 
 
-
 int main(){
-    vector<bool> arr;
-    int a,b,c;
-    cin>>a>>b>>c;
-    cout<<makeItEqual(a,b,c);
+    int n;
+    cin>>n;
+    
+    Node* head = new Node(-1);
+    Node* ans = head;
+    for(int i=0;i<n;i++){
+     int val;
+     cin>>val;
+     Node *temp = new Node(val);
+     ans->next = temp;
+     ans = ans->next;
+    }
+    
+    Node* res = segregate(n,head->next);
+    display(res);
     return 0;
 }
